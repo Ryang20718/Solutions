368c963dff23fd752cb903ac4e96176496193d73
Ryan Yang <Ryanyang99@hotmail.com>
2018-11-18 22:41:44 -0800

    update

---
+++ Easy-/Linkedlist/ReverseSingle.py
@@ -0,0 +1,21 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def reverseList(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        prev = None
+        cur = head
+        while(cur != None):
+            sec = cur.next
+            cur.next = prev
+            prev = cur
+            cur = sec
+        return prev
+            
